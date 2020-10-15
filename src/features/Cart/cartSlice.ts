c8430a82edd4c3d15a9efa0408b51cfb091fb95e
SharjeelSafdar <SharjeelSafdar@users.noreply.github.com>
2020-10-16 01:54:22 +0500

    Removed dummy cart items

--- src/features/Cart/cartSlice.ts
+++ src/features/Cart/cartSlice.ts
@@ -1,24 +1,24 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
 const initialState: CartItem[] = [
-    {
-        id: "3f60de24-1815-4d88-a8dc-5ceda3f41bdc",
-        name: "Air Jordan 1 Mid",
-        category: "Men's Shoes",
-        price: 115,
-        image: "https://static.nike.com/a/images/t_PDP_1280_v1/f_auto/i1-d2f41ddc-a08e-443a-8eb0-6960ebb4a408/air-jordan-1-mid-shoe-1zMCFJ.jpg",
-        quantity: 1,
-        includedInSum: false,
-    },
-    {
-        id: "d52fd362-1080-46b3-a43c-d64f7a6825ab",
-        name: "Nike Air Zoom-Type",
-        category: "Men's Shoes",
-        price: 150,
-        image: "https://static.nike.com/a/images/t_PDP_1280_v1/f_auto/5c1e3a90-b2b7-479c-b567-3b50903cf9da/air-zoom-type-mens-shoe-PZR40V.jpg",
-        quantity: 3,
-        includedInSum: true,
-    },
+    // {
+    //     id: "3f60de24-1815-4d88-a8dc-5ceda3f41bdc",
+    //     name: "Air Jordan 1 Mid",
+    //     category: "Men's Shoes",
+    //     price: 115,
+    //     image: "https://static.nike.com/a/images/t_PDP_1280_v1/f_auto/i1-d2f41ddc-a08e-443a-8eb0-6960ebb4a408/air-jordan-1-mid-shoe-1zMCFJ.jpg",
+    //     quantity: 1,
+    //     includedInSum: false,
+    // },
+    // {
+    //     id: "d52fd362-1080-46b3-a43c-d64f7a6825ab",
+    //     name: "Nike Air Zoom-Type",
+    //     category: "Men's Shoes",
+    //     price: 150,
+    //     image: "https://static.nike.com/a/images/t_PDP_1280_v1/f_auto/5c1e3a90-b2b7-479c-b567-3b50903cf9da/air-zoom-type-mens-shoe-PZR40V.jpg",
+    //     quantity: 3,
+    //     includedInSum: true,
+    // },
 ];
 
 const getItemIndex = (state: CartItem[], idToFind: string): number => {
